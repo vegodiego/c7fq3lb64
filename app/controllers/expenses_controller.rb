7e4b3b695d9a57a507ddf6bc0760c9e5ef836d9f
root <root@LAPTOP-8DE7N6T3.localdomain>
2019-11-30 20:45:30 -0500

    Agregando la funcionalidad filtrar los gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,17 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    
+    @expenses=Expense.all
+    if params[:category_id] != "" && params[:concept] != ""
+      @expenses=Expense.all.select {|i| i.category_id==params[:category_id].to_i && i.concept.upcase.include?(params[:concept].upcase)}
+    elsif params[:category_id] != "" && params[:concept] == ""
+      @expenses=Expense.where(category_id: params[:category_id])
+    elsif params[:category_id] == "" && params[:concept] != ""
+      @expenses=Expense.all.select {|i| i.concept.upcase.include?(params[:concept].upcase)}
+    end
+
+     
+    
   end
 end
+
