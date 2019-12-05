17490b32638120239285aa8acdbe5b8b9257f544
root <root@LAPTOP-8DE7N6T3.localdomain>
2019-12-05 17:32:25 -0500

    solucionando el problema de expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,14 +3,19 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @expenses=current_user.expenses
-    if params[:category_id] != "" && params[:concept] != ""
-      @expenses=current_user.expenses.select {|i| i.category_id==params[:category_id].to_i && i.concept.upcase.include?(params[:concept].upcase)}
-    elsif params[:category_id] != "" && params[:concept] == ""
-      @expenses=current_user.expenses.where(category_id: params[:category_id])
-    elsif params[:category_id] == "" && params[:concept] != ""
-      @expenses=current_user.expenses.select {|i| i.concept.upcase.include?(params[:concept].upcase)}
-    end
+    if params[:category_id]
+      if params[:category_id] != "" && params[:concept] != ""
+        @expenses=current_user.expenses.select {|i| i.category_id==params[:category_id].to_i && i.concept.upcase.include?(params[:concept].upcase)}
+      elsif params[:category_id] != "" && params[:concept] == ""
+        @expenses=current_user.expenses.where(category_id: params[:category_id])
+      elsif params[:category_id] == "" && params[:concept] != ""
+        @expenses=current_user.expenses.select {|i| i.concept.upcase.include?(params[:concept].upcase)}
+      elsif params[:category_id] == "" && params[:concept] == ""
+        @expenses=current_user.expenses
+      end
+    elsif
+      @expenses=current_user.expenses
+    end    
   end
 end
 
